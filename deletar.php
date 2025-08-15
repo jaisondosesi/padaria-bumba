c78e3204a4b957724f4db8f57c6e367959e514c9
Julia Izabel <julia_roza@estudante.sesisenai.org.br>
2025-08-15 08:53:57 -0300

    add deletar

---
+++ deletar.php
@@ -0,0 +1,14 @@
+<?php
+include 'conexao.php';
+
+$id = $_GET['id'];
+$sql = "DELETE FROM produtos WHERE id_produto = $id";
+
+if ($conn->query($sql) === TRUE) {
+    header("Location: index.php");
+} else {
+    echo "Erro: " . $conn->error;
+}
+?>
+
+
